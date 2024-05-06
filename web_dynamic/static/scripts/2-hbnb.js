71f3e3085726aaad301115d64a661b4bc2768c75
Selasi-Mensah <mensahselasi78@gmail.com>
2024-05-06 15:17:25 +0000

    Done

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,20 +1,26 @@
-document.ready(function () {
-	const amenities = {};
-	$("li input[type=checkbox]").change(function () {
-		if (this.checked) {
-			amenities[this.dataset.name] = this.dataset.id;
-		} else {
-			delete amenities[this.dataset.name];
-		}
-		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+window.addEventListener('load', function () {
+	// task 3
+	$.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
+	  if (data.status === 'OK') {
+		$('#api_status').addClass('available');
+	  } else {
+		$('#api_status').removeClass('available');
+	  }
 	});
-
-	// get status of API
-	$.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
-		if (data.status === "OK") {
-			$("div#api_status").addClass("available");
-		} else {
-			$("div#api_status").removeClass("available");
-		}
+  
+	// task 2
+	const amenityIds = {};
+	$('input[type=checkbox]').click(function () {
+	  if ($(this).prop('checked')) {
+		amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+	  } else if (!$(this).prop('checked')) {
+		delete amenityIds[$(this).attr('data-id')];
+	  }
+	  if (Object.keys(amenityIds).length === 0) {
+		$('div.amenities h4').html('&nbsp');
+	  } else {
+		$('div.amenities h4').text(Object.values(amenityIds).join(', '));
+	  }
 	});
-});
+  });
+  
